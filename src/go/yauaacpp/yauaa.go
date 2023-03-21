0da3294a2429cb98e3fe1cf53725253ca0af7f07
sunxiangguang <717095300@qq.com>
2023-03-21 14:40:01 +0800

    example go change to 19 browser

--- src/go/yauaacpp/yauaa.go
+++ src/go/yauaacpp/yauaa.go
@@ -6,6 +6,26 @@ package yauaacpp
 #include "string.h"
 #include "yauaacpp/yauaa_c.h"
 
+#define BROWSER_OTHER       0
+#define BROWSER_CHROME      1
+#define BROWSER_MOZILLA     2
+#define BROWSER_SAFARI      3
+#define BROWSER_IE          4
+#define BROWSER_EDGE        5
+#define BROWSER_SAMSUNG     6
+#define BROWSER_MINUI       7
+#define BROWSER_OPERA       8
+#define BROWSER_YANDEX      9
+#define BROWSER_UC          10
+#define BROWSER_FIREFOX     11
+#define BROWSER_HUAWEI      12
+#define BROWSER_WEBVIEW             20
+#define BROWSER_WEBVIEW_FACEBOOK    21
+#define BROWSER_WEBVIEW_INSTAGRAM   22
+#define BROWSER_WEBVIEW_SNAPCHAT    23
+#define BROWSER_WEBVIEW_GOOGLE      24
+#define BROWSER_GOOGLEBOT           30
+
 typedef struct {
 	void * uaa;
 	char * ua;
@@ -14,7 +34,7 @@ typedef struct {
 }uainfo;
 
 USER_AGENT_ANALYSER create_envirenment_browser_uaa(int cache_size) {
-	char * fields[] = { DEVICE_CLASS, AGENT_NAME, NULL };
+	char * fields[] = { DEVICE_CLASS, AGENT_NAME, WEBVIEW_APP_NAME, NULL };
 	return user_agent_analyser_create(fields, cache_size);
 }
 
@@ -32,18 +52,50 @@ int get_envirenment_browser(uainfo * info){
         info->evironment = 0;
 
     const char * agentName = results[1];
-    if(0 == strcmp(agentName, "Chrome"))
-        info->browser = 1;
-    else if(0 == strcmp(agentName, "Mozilla"))
-        info->browser = 2;
-    else if(0 == strcmp(agentName, "Safari"))
-        info->browser = 3;
-    else if(0 == strcmp(agentName, "Internet Explorer"))
-        info->browser = 4;
-    else if(0 == strcmp(agentName, "Edge"))
-        info->browser = 5;
+    const char * webviewApp = results[2];
+    if(0 == strcmp(agentName,"Chrome"))
+        info->browser = BROWSER_CHROME;
+    else if(0 == strcmp(agentName,"Mozilla"))
+        info->browser = BROWSER_MOZILLA;
+    else if(0 == strcmp(agentName,"Safari"))
+        info->browser = BROWSER_SAFARI;
+    else if(0 == strcmp(agentName,"Internet Explorer"))
+        info->browser = BROWSER_IE;
+    else if(0 == strcmp(agentName,"Edge") || 0 == strcmp(agentName,"Edge Webview"))
+        info->browser = BROWSER_EDGE;
+    else if(0 == strcmp(agentName,"SamsungBrowser") || 0 == strcmp(webviewApp,"SamsungBrowser"))
+        info->browser = BROWSER_SAMSUNG;
+    else if(0 == strcmp(agentName,"Opera") || 0 == strcmp(webviewApp,"Opera"))
+        info->browser = BROWSER_OPERA;
+    else if(0 == strcmp(agentName,"Firefox"))
+        info->browser = BROWSER_FIREFOX;
+    else if(0 == strcmp(agentName,"MiuiBrowser"))
+        info->browser = BROWSER_MINUI;
+    else if(0 == strcmp(agentName,"YaBrowser"))
+        info->browser = BROWSER_YANDEX;
+    else if(0 == strcmp(agentName,"HuaweiBrowser"))
+        info->browser = BROWSER_HUAWEI;
+    else if(0 == strcmp(agentName,"Chrome Webview") || 0 == strcmp(agentName,"UIWebView") || 0 == strcmp(agentName,"AppleWebKit") ||
+            0 == strcmp(agentName,"NewsBreak") ||
+            strstr(agentName,"WebViewApp") == agentName) // start with "WebViewApp"
+        info->browser = BROWSER_WEBVIEW;
+    else if(0 == strcmp(deviceClass,"Robot") || strstr(agentName,"Googlebot") == agentName)
+        info->browser = BROWSER_GOOGLEBOT;
     else
-        info->browser = 0;
+        info->browser = BROWSER_OTHER;
+
+    if(info->browser == BROWSER_OTHER || info->browser == BROWSER_WEBVIEW) {
+        if (0 == strcmp(webviewApp,"AgentWeb"))
+            info->browser = BROWSER_UC;
+        else if (0 == strcmp(webviewApp,"Facebook App for Android") || 0 == strcmp(webviewApp,"Facebook App for iOS") || 0 == strcmp(webviewApp,"Facebook EMA"))
+            info->browser = BROWSER_WEBVIEW_FACEBOOK;
+        else if (0 == strcmp(agentName,"Instagram") || 0 == strcmp(webviewApp,"Instagram"))
+            info->browser = BROWSER_WEBVIEW_INSTAGRAM;
+        else if (0 == strcmp(webviewApp,"Google Search App"))
+            info->browser = BROWSER_WEBVIEW_GOOGLE;
+        else if (0 == strcmp(webviewApp,"Snapchat"))
+            info->browser = BROWSER_WEBVIEW_SNAPCHAT;
+    }
 	return 1;
 }
 */
